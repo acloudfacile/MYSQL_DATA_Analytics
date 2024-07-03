12d3f81ca4793a442d79f0e26a9424764367bb14
Harsha Vardhan Reddy <acloudfacile@pm.me>
2024-07-03 22:44:21 +0900

    Select Command Commit

---
+++ Select.sql
@@ -0,0 +1,20 @@
+-- Select Statement cases
+USE bakery;
+
+Select * 
+FROM customers;
+
+SELECT first_name, 
+last_name, 
+birth_date, 
+phone, 
+city, 
+state,
+total_money_spent,
+(total_money_spent + 100) * 10
+FROM customers;
+
+-- PEMDAS - parenthesis, exponent, multiplication, division, addition and subtraction.
+
+SELECT DISTINCT state, city
+FROM customers;
